5e27e4cb191a592e1b0709334ee21316d15504a2
Endale24 <endale2400@gmail.com>
2022-06-13 07:38:56 -0700

    commit changes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,26 @@
  */
 void rev_string(char *s)
 {
-int temp, i, j, length;
-while (s[i] != 0)
-{
-i++;
-}
+	int i;
+	int j;
+	int temp;
+	int length;
 
-length = i;
-i = 0;
-j= length - 1;
-while (i < j)
-{
-temp = s[i];
-s[i] = s[j];
-s[j] = temp;
-i++;
-j--;
-}
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	length = i;
+	i = 0;
+	j = length - 1;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
 }
