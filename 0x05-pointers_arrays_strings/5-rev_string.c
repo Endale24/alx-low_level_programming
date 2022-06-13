063d50c3d8e710f4434803781b4cf2cbea8e7e21
Endale24 <endale2400@gmail.com>
2022-06-13 07:25:10 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: holi
+ *
+ */
+void rev_string(char *s)
+{
+int temp, i, j, length;
+while (s[i] != 0)
+{
+i++;
+}
+
+length = i;
+i = 0;
+j= length - 1;
+while (i < j)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+i++;
+j--;
+}
+}
