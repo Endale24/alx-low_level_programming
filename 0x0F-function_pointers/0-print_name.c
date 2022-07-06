b88053ed1c568b4c0808ac8cba1c42f8a8a88bc8
Endale24 <endale2400@gmail.com>
2022-07-06 10:29:46 -0700

    recorrecting

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,17 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
- * print_name - Print a name from a function pointer
- * @name: char string
- * @f: function pointer that takes a string argument
- */
-
+  * print_name - initializes a variable
+  *
+  * @name: name.
+  * @f: pointer.
+  * Return: nothing.
+  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+	
 	f(name);
 }
